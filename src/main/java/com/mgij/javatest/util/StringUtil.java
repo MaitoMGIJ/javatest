ffb07184e94637bae3b62d2305aa3e5227b7cfb8
Mauricio Avila Beltran <mavilab@ayesa.es>
2021-04-16 20:24:37 -0500

    Different test cases

--- src/main/java/com/mgij/javatest/util/StringUtil.java
+++ src/main/java/com/mgij/javatest/util/StringUtil.java
@@ -2,6 +2,9 @@ package com.mgij.javatest.util;
 
 public class StringUtil {
     public static String repeat(String str, int times){
+        if(times < 0){
+            throw new IllegalArgumentException("Negative time not allowed");
+        }
         String result = "";
         for (int i = 0; i < times; i++) {
             result += str;
