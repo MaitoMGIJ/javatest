824e20caf11092b2cd4d82e4ecaa5f15115db6e6
Mauricio Avila Beltran <mavilab@ayesa.es>
2021-04-17 06:42:44 -0500

    Test method isEmpty

--- src/test/java/com/mgij/javatest/util/StringUtilTest.java
+++ src/test/java/com/mgij/javatest/util/StringUtilTest.java
@@ -1,28 +1,48 @@
 package com.mgij.javatest.util;
 
-import org.junit.Assert;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class StringUtilTest {
 
     @Test
     public void repeat_string_once() {
-        Assert.assertEquals("hola", StringUtil.repeat("hola", 1));
+        assertEquals("hola", StringUtil.repeat("hola", 1));
     }
 
     @Test
     public void repeat_string_multiples_times() {
-        Assert.assertEquals("holaholahola", StringUtil.repeat("hola", 3));
+        assertEquals("holaholahola", StringUtil.repeat("hola", 3));
     }
 
     @Test
     public void repeat_string_zero_times() {
-        Assert.assertEquals("", StringUtil.repeat("hola", 0));
+        assertEquals("", StringUtil.repeat("hola", 0));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void repeat_string_negative_times() {
         StringUtil.repeat("hola", -1);
+    }
+
+    @Test
+    public void string_is_not_empty(){
+        assertFalse(StringUtil.isEmpty("string_not_empty"));
+    }
+
+    @Test
+    public void string_empty_is_empty(){
+        assertTrue(StringUtil.isEmpty(""));
+    }
+
+    @Test
+    public void null_is_empty(){
+        assertTrue(StringUtil.isEmpty(null));
+    }
+
+    @Test
+    public void string_with_spaces_is_empty(){
+        assertTrue(StringUtil.isEmpty(" "));
     }
 
 }
